0d483b5341482fccaf1861c3145271023e6a8c8d
Muhammad Ahmad <93067257+MuhammadAhmadAk@users.noreply.github.com>
2023-08-14 01:50:25 -0700

    updated

--- lib/Views/splash screen/splash_screen.dart
+++ lib/Views/splash screen/splash_screen.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+import '../auth/login_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   static const routeName = '/';
@@ -11,13 +15,28 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    navigateToHome();
+    super.initState();
+  }
+
+  void navigateToHome() async {
+    Timer.periodic(Duration(seconds: 3), (timer) {
+      Navigator.of(context).pushReplacementNamed(LoginScreen.routeName);
+      timer.cancel();
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blue,
       body: SafeArea(
           child: Center(
         child: Text(
           "Chat App",
-          style: GoogleFonts.kanit(fontSize: 50, fontWeight: FontWeight.bold),
+          style: GoogleFonts.kanit(
+              fontSize: 50, fontWeight: FontWeight.bold, color: Colors.white),
         ),
       )),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_firebase/Views/auth/login_screen.dart';
+import 'package:flutter_firebase/Utils/routes.dart';
 import 'package:flutter_firebase/bloc/auth-cubit/auth_cubit.dart';
 import 'package:flutter_firebase/firebase_options.dart';
 
@@ -17,16 +17,21 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => AuthCubit(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => AuthCubit(),
+        )
+      ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
+        title: 'Chat App',
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: const LoginScreen(),
+        initialRoute: '/',
+        onGenerateRoute: generateRoute,
       ),
     );
   }
